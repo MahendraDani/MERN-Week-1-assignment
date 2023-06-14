ebefd22fcbc0e78091b95eb31bd3eeefb61819f8
Mahendra Dani <danimahendra0904@gmail.com>
2023-06-14 13:08:02 +0530

    solved anagram problem

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,29 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
+function removeBlankSpace(value) {
+  if (value !== " ") {
+    return true;
+  } else {
+    return false;
+  }
+}
 function isAnagram(str1, str2) {
-
+  if (str1 === "" && str2 === "") {
+    return true;
+  }
+  const arr1 = str1.toLowerCase().split("").filter(removeBlankSpace);
+  const arr2 = str2.toLowerCase().split("").filter(removeBlankSpace);
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      return true;
+    } else return false;
+  }
 }
 
+ans = isAnagram("Ssame", "ms sae");
+console.log(ans);
 module.exports = isAnagram;
